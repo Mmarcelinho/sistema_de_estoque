65420fa427da7b351561434fb915528caec42de5
Mmarcelinho <marcelorosario2001@gmail.com>
2024-05-06 18:17:30 -0300

    Implementação da interface da entidade Admin

--- src/Backend/SistemaDeEstoque.Domain/Repositorios/Admin/IAdminReadOnlyRepositorio.cs
+++ src/Backend/SistemaDeEstoque.Domain/Repositorios/Admin/IAdminReadOnlyRepositorio.cs
@@ -1,5 +1,12 @@
 namespace SistemaDeEstoque.Domain.Repositorios.Admin;
 
-    public interface IAdminReadOnlyRepositorio
-    {
-    }
+public interface IAdminReadOnlyRepositorio
+{
+    Task<bool> ExisteAdminComEmail(string email);
+
+    Task<Entidades.Admin> RecuperarPorId(long id);
+
+    Task<Entidades.Admin> RecuperarPorEmailSenha(string email, string senha);
+
+    Task<Entidades.Admin> RecuperarPorEmail(string email);
+}
