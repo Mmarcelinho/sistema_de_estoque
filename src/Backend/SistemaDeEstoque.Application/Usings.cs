38396942460042a85c1d9889f9b08e3df79691fb
Mmarcelinho <marcelorosario2001@gmail.com>
2024-05-08 20:45:00 -0300

    Adiciona chave adicional no appsettings

--- src/Backend/SistemaDeEstoque.Application/Usings.cs
+++ src/Backend/SistemaDeEstoque.Application/Usings.cs
@@ -1,7 +1,16 @@
-global using FluentValidation;
+global using System.Reflection;
+global using System.Text;
+global using System.Security.Cryptography;
+
 global using MediatR;
+global using FluentValidation;
+
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Configuration;
-global using System.Reflection;
+
 global using SistemaDeEstoque.Application.Servicos.Validation;
+global using SistemaDeEstoque.Application.Servicos.Criptografia;
+
+
+
 
