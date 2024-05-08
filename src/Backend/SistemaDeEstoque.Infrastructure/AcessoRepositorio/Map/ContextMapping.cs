0980cd4256690261f59c96ac278b81c07c3f3864
Mmarcelinho <marcelorosario2001@gmail.com>
2024-05-07 23:55:37 -0300

    Adiciona método para recuperar tabela da entidade Admin

---
+++ src/Backend/SistemaDeEstoque.Infrastructure/AcessoRepositorio/Map/ContextMapping.cs
@@ -0,0 +1,6 @@
+namespace SistemaDeEstoque.Infrastructure.AcessoRepositorio.Map;
+
+    public static class ContextMapping
+    {
+        public static string RecuperarTabelaAdmin() => "Admins";
+    }
