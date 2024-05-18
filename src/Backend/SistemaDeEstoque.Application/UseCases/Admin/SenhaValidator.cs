c445788f37110d385dc865d1105fe2cb4fb621df
Mmarcelinho <marcelorosario2001@gmail.com>
2024-05-18 15:01:21 -0300

    Adiciona validação de senha

---
+++ src/Backend/SistemaDeEstoque.Application/UseCases/Admin/SenhaValidator.cs
@@ -0,0 +1,17 @@
+using SistemaDeEstoque.Exceptions.ErrorMessages;
+
+namespace SistemaDeEstoque.Application.UseCases.Admin;
+
+    public class SenhaValidator : AbstractValidator<string>
+    {
+        public SenhaValidator()
+        {
+            RuleFor(senha => senha).NotEmpty().WithMessage(UsuarioModelMensagensDeErro.SENHA_USUARIO_EMBRANCO);
+            When(senha => !string.IsNullOrWhiteSpace(senha), () => 
+            {
+                RuleFor(senha => senha.Length)
+                .GreaterThanOrEqualTo(6)
+                .WithMessage(UsuarioModelMensagensDeErro.SENHA_USUARIO_MINIMO_SEIS_CARACTERES);
+            });
+        }
+    }
