976472863f7116b6d8c2e80f50a8a1a7ca5e6ac5
Mmarcelinho <marcelorosario2001@gmail.com>
2024-05-24 22:53:48 -0300

    Altera validação de RegistrarAdmin para usar RegistrarAdminCommand

--- src/Backend/SistemaDeEstoque.Application/UseCases/Admin/Registrar/RegistrarAdminValidator.cs
+++ src/Backend/SistemaDeEstoque.Application/UseCases/Admin/Registrar/RegistrarAdminValidator.cs
@@ -1,22 +1,20 @@
-using System.Text.RegularExpressions;
-
 namespace SistemaDeEstoque.Application.UseCases.Admin.Registrar;
 
-public class RegistrarAdminValidator : AbstractValidator<RequisicaoRegistrarAdminJson>
+public class RegistrarAdminValidator : AbstractValidator<RegistrarAdminCommand>
 {
     public RegistrarAdminValidator()
     {
-        RuleFor(c => c.Nome).NotEmpty().WithMessage(UsuarioModelMensagensDeErro.NOME_USUARIO_EMBRANCO);
-        RuleFor(c => c.Email).NotEmpty().WithMessage(UsuarioModelMensagensDeErro.EMAIL_USUARIO_EMBRANCO);
-        RuleFor(c => c.Telefone).NotEmpty().WithMessage(UsuarioModelMensagensDeErro.TELEFONE_USUARIO_EMBRANCO);
-        RuleFor(c => c.Senha).SetValidator(new SenhaValidator());
-        When(c => !string.IsNullOrWhiteSpace(c.Email), () =>
+        RuleFor(c => c.registrarAdmin.Nome).NotEmpty().WithMessage(UsuarioModelMensagensDeErro.NOME_USUARIO_EMBRANCO);
+        RuleFor(c => c.registrarAdmin.Email).NotEmpty().WithMessage(UsuarioModelMensagensDeErro.EMAIL_USUARIO_EMBRANCO);
+        RuleFor(c => c.registrarAdmin.Telefone).NotEmpty().WithMessage(UsuarioModelMensagensDeErro.TELEFONE_USUARIO_EMBRANCO);
+        RuleFor(c => c.registrarAdmin.Senha).SetValidator(new SenhaValidator());
+        When(c => !string.IsNullOrWhiteSpace(c.registrarAdmin.Email), () =>
         {
-            RuleFor(c => c.Email).EmailAddress().WithMessage(UsuarioModelMensagensDeErro.EMAIL_USUARIO_INVALIDO);
+            RuleFor(c => c.registrarAdmin.Email).EmailAddress().WithMessage(UsuarioModelMensagensDeErro.EMAIL_USUARIO_INVALIDO);
         });
-        When(c => !string.IsNullOrWhiteSpace(c.Telefone), () =>
+        When(c => !string.IsNullOrWhiteSpace(c.registrarAdmin.Telefone), () =>
         {
-            RuleFor(c => c.Telefone).Custom((telefone, contexto) =>
+            RuleFor(c => c.registrarAdmin.Telefone).Custom((telefone, contexto) =>
             {
                 string padraoTelefone = "[0-9]{2} [1-9]{1} [0-9]{4}-[0-9]{4}";
                 var isMatch = Regex.IsMatch(telefone, padraoTelefone, RegexOptions.None, TimeSpan.FromMilliseconds(100));
