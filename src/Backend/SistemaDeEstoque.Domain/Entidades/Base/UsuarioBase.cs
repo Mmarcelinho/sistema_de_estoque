bb588fe0b1c179d4dacf86c9e94299d305179aae
Mmarcelinho <marcelorosario2001@gmail.com>
2024-05-26 20:52:54 -0300

    Adiciona propriedade Role e string vazia nas propriedades tipo string

--- src/Backend/SistemaDeEstoque.Domain/Entidades/Base/UsuarioBase.cs
+++ src/Backend/SistemaDeEstoque.Domain/Entidades/Base/UsuarioBase.cs
@@ -2,11 +2,16 @@ namespace SistemaDeEstoque.Domain.Entidades.Base;
 
 public abstract class Usuario : EntidadeBase
 {
-    public string Nome { get; set; }
+    public string Nome { get; set; } = string.Empty;
 
-    public string Email { get; set; }
+    public string Email { get; set; } = string.Empty;
 
-    public string Senha { get; set; }
+    public string Senha { get; set; } = string.Empty;
 
-    public string Telefone { get; set; }
+    public string Telefone { get; set; } = string.Empty;
+
+    public Guid IdentificadorUsuario { get; set; }
+
+    public string Role { get; set; } = string.Empty;
+
 }
