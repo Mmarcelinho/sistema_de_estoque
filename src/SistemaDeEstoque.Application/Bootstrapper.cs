87727b18428b7709a021137defcc9488e5cc023a
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-17 22:38:32 -0300

    Remove código inutilizado

--- src/SistemaDeEstoque.Application/Bootstrapper.cs
+++ src/SistemaDeEstoque.Application/Bootstrapper.cs
@@ -4,17 +4,7 @@ public static class Bootstrapper
 {
     public static void AdicionarApplication(this IServiceCollection services, IConfiguration configuration)
     {
-        AdicionarTokenJWT(services, configuration);
         AdicionarMediatR(services);
-        AdicionarUsuarioLogado(services);
-    }
-
-    private static void AdicionarTokenJWT(IServiceCollection services, IConfiguration configuration)
-    {
-        var sectionTempoDeVida = configuration.GetRequiredSection("Configuracoes:Jwt:TempoVidaTokenMinutos");
-        var sectionKey = configuration.GetRequiredSection("Configuracoes:Jwt:ChaveToken");
-
-        services.AddScoped(option => new TokenController(int.Parse(sectionTempoDeVida.Value), sectionKey.Value));
     }
 
     private static void AdicionarMediatR(IServiceCollection services)
@@ -29,7 +19,4 @@ public static class Bootstrapper
 
         services.AddValidatorsFromAssembly(Assembly.Load("SistemaDeEstoque.Application"));
     }
-
-    private static void AdicionarUsuarioLogado(IServiceCollection services)
-    => services.AddScoped<IAdminLogado, AdminLogado>();
 }
